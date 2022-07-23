1d99f78cd6235f9fe1fb5ddca2092b8a4a18f66e
Rosgrad <109528856+Rosgrad@users.noreply.github.com>
2022-07-23 13:56:19 +0300

    Add files via upload

---
+++ Testeven/Testeven/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Testeven
+{
+    internal class Program
+    {   
+        static void Main(string[] args)
+        {
+            int b;
+            Console.WriteLine("Есть два варианта решения: ");
+            Console.WriteLine("Выберите вариант 1 или 2: ");
+            b = Convert.ToInt32(Console.ReadLine());
+            if (b==1)
+            Var1();
+            else if (b==2)
+             Var2();
+            else if (b>2)
+           Console.WriteLine("Нет такого варината решения!!! ");
+        }
+
+        static void Var1()
+        {
+            int a;
+            Console.WriteLine("введите число: ");
+            a = Convert.ToInt32(Console.ReadLine());
+            if (a % 2 == 0)
+            {
+                Console.WriteLine("четное число");
+            }
+            else
+            {
+                Console.WriteLine("нечетное");
+            }
+            Console.ReadKey();
+        }
+        static void Var2()
+        {
+            int a;
+            Console.WriteLine("введите число: ");
+            a = int.Parse(Console.ReadLine());
+            int temp = a % 2;
+            bool result = temp == 0;
+            if (result)
+            {
+                Console.WriteLine("четное число");
+            }
+            else
+            {
+                Console.WriteLine("нечетное");
+            }
+            Console.ReadKey();
+        }
+         
+    }
+   
+    }
+
+ 
